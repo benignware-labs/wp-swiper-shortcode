0129c09f7904fa33d33061b4300bb3fcc25ced15
Rafael Nowrotek <mail@benignware.com>
2016-11-20 18:18:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# wp-swiper-shortcode
+
+Swiper Shortcode Integration for Wordpress
+
+## Usage
+
+```
+[swiper autoplay=3000]
+  [swiper_slide]
+    Slide 1
+  [/swiper_slide]
+  [swiper_slide]
+    Slide 2
+  [/swiper_slide]
+[/siper]
+```
---
+++ wp_swiper_shortcode.php
@@ -0,0 +1,133 @@
+<?php
+/*
+Plugin Name: WP Swiper Shortcode
+Plugin URI: http://wordpress.org/extend/plugins/wp-swiper-shortcode
+Description: Integrates swiper with wordpress shortcode
+Version: 0.0.1
+Author: Rafael Nowrotek
+Author URI: http://benignware.com
+Author Email: mail@benignware.com
+Text Domain: swiper
+Domain Path: /lang/
+Network: false
+License: MIT
+License URI: https://opensource.org/licenses/MIT
+
+Copyright 2016 benignware.com
+*/
+function wp_swiper_shortcode_enqueue_scripts() {
+  $vendor_assets_dir = 'assets';
+  wp_enqueue_script( 'swiper', plugin_dir_url( __FILE__ ) . $vendor_assets_dir . "Swiper/dist/js/swiper.jquery.js", array( 'jquery' ) );
+  wp_enqueue_style( 'swiper', plugin_dir_url( __FILE__ ) . $vendor_assets_dir . "Swiper/dist/css/swiper.css");
+}
+
+add_action( 'wp_enqueue_scripts', 'wp_swiper_shortcode_enqueue_scripts' );
+
+
+function wp_swiper_shortcode($atts = array(), $content = "") {
+  $html_atts = array('id', 'class');
+
+  $atts = shortcode_atts(array(
+    'id' => 'swiper-' . uniqid(),
+    'class' => '',
+    'pagination' => '.swiper-pagination',
+    'paginationClickable' =>  true,
+    'nextButton' => '.swiper-button-next',
+    'prevButton' => '.swiper-button-prev',
+    'scrollbar' => false
+  ), $atts, 'swiper');
+
+  $atts['class'].= strpos($atts['class'], 'swiper-container') === false ? ' swiper-container' : '';
+
+  $html_atts =
+  // TODO: Camelize recursively
+  $options = copy($atts);
+
+  $json = json_encode($options, JSON_UNESCAPED_SLASHES);
+
+  // Create output
+  $output = "";
+  $output.= "<div";
+  foreach ($atts as $name => $value) {
+    $output.= ' ' . $name . '="' . $value . '"';
+  }
+  $output.= ">";
+  $output.= "<div class=\"swiper-wrapper\">";
+  $output.= do_shortcode($content);
+  $output.= "</div>";
+  if ($atts['pagination']) {
+    $output.= '<div class="swiper-pagination"></div>';
+  }
+  if ($atts['nextButton']) {
+    $output.= '<div class="swiper-button-next"></div>';
+  }
+  if ($atts['prevButton']) {
+    $output.= '<div class="swiper-button-prev"></div>';
+  }
+  if ($atts['scrollbar']) {
+    $output.= '<div class="swiper-scrollbar"></div>';
+  }
+  $output.= "</div>";
+  $output.= "<script type=\"text/javascript\">//<![CDATA[\n(function($, window) {\n";
+  $output.= "\t$('#{$atts['id']}').swiper(" . $json . ");\n";
+  $output.= "})(jQuery, window)\n//]]></script>\n";
+  return $output;
+}
+
+add_shortcode('swiper', 'wp_swiper_shortcode');
+
+function wp_swiper_slide_shortcode($atts = array(), $content = "") {
+  $swiper_slide_options = array('');
+  $atts = shortcode_atts(array(
+    'class' => 'swiper-slide',
+    'before_content' => "<div class='swiper-slide-content'>",
+    'after_content' => '</div>',
+  ), $atts, 'swiper_slide');
+
+  $atts['class'].= strpos($atts['class'], 'swiper-container') === false ? ' swiper-slide' : '';
+
+  $options = array();
+  foreach ($atts as $name => $value) {
+    if (in_array($name, $swiper_slide_options) === true) {
+      unset($atts[$name]);
+      $options[$name] = $value;
+    }
+  }
+  $json = json_encode($options, JSON_UNESCAPED_SLASHES);
+
+  // Create output
+  $output = "";
+  $output.= "<div";
+  foreach ($atts as $name => $value) {
+    $output.= ' ' . $name . '="' . $value . '"';
+  }
+  $output.= ">";
+
+  $output.= $atts['before_content'];
+  $output.= do_shortcode( $content );
+  $output.= $atts['after_content'];
+
+  $output.= "</div>";
+  return $output;
+}
+
+add_shortcode('swiper_slide', 'wp_swiper_slide_shortcode');
+
+function wp_swiper_shortcode_empty_paragraph_fix( $content ) {
+
+    $shortcodes = array( 'swiper', 'swiper_slide' );
+    foreach ( $shortcodes as $shortcode ) {
+        $array = array (
+            '<p>[' . $shortcode => '[' .$shortcode,
+            '<p>[/' . $shortcode => '[/' .$shortcode,
+            $shortcode . ']</p>' => $shortcode . ']',
+            $shortcode . ']<br />' => $shortcode . ']'
+        );
+        $content = strtr( $content, $array );
+    }
+
+    return $content;
+}
+
+add_filter( 'the_content', 'wp_swiper_shortcode_empty_paragraph_fix' );
+?>
