be28c2cbb6f38739eb4cf9c83b881867dc359cbf
Rafael Nowrotek <mailbenignware.com>
2019-03-31 15:51:40 +0200

    Fix version mess

--- swiper-shortcode.php
+++ swiper-shortcode.php
@@ -3,7 +3,7 @@
 Plugin Name: Swiper Shortcode
 Plugin URI: https://github.com/benignware-labs/wp-swiper-shortcode
 Description: Swiper integration for Wordpress
-Version: 0.0.11s
+Version: 0.0.12
 Author: Rafael Nowrotek
 Author URI: http://benignware.com
 Author Email: mail@benignware.com
